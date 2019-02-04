a41904c2e9dfbd739004d7c9f5aa0eb87ca2e09e
babojamo <kevin.loquencio@gmail.com>
2019-02-04 16:41:02 +0800

    Adding components

--- src/SintexServiceProvider.php
+++ src/SintexServiceProvider.php
@@ -15,9 +15,9 @@ class SintexServiceProvider extends ServiceProvider
             __DIR__.'/views' => base_path('resources/views/sintex'),
         ]);
 
-
-        Blade::component('sintex::layout', 'sintexlayout');
-
+        Blade::component('sintex::layouts.sidebar', 'sintexlayoutside');
+        Blade::component('sintex::layouts.top-nav', 'sintexlayouttop');
+        Blade::component('sintex::layouts.top-nav', 'sintexemail');
     }
     public function register()
     {
